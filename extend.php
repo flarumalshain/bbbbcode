0f3e64ed845d31578cb721146f6b3f293b390698
Bungalow Bill <41351317+zerosonesfun@users.noreply.github.com>
2019-02-23 18:08:10 -0500

    Update extend.php

--- extend.php
+++ extend.php
@@ -21,22 +21,22 @@ return [
     (new Extend\Formatter)
         ->configure(function (Configurator $config) {
             $config->BBCodes->addCustom(
-                '[tooltip="{TEXT1}"]{TEXT2}[/tooltip]',
-                '<span class="bb-tooltip" data-tooltip="{TEXT1}" data-placement="right">{TEXT2}</span>'
+                '[tooltip="{TEXT1}" placement="{TEXT2}]{TEXT3}[/tooltip]',
+                '<span class="bb-tooltip" data-tooltip="{TEXT1}" data-placement="{TEXT2}">{TEXT3}</span>'
             );
             $config->BBCodes->addCustom(
-                '[accordian header="{TEXT3}"]{TEXT4}[/accordian]',
+                '[accordian header="{TEXT4}"]{TEXT5}[/accordian]',
                 '<div class="accordion">
                     <input type="radio" name="radacc" class="accordion-chk" />
-                       <h3 class="accordion-header">
-                       {TEXT3}
-                       <span class="acc-icon"></span>
-                       </h3>
-                       <div class="accordion-content">
-                       <p>
-                       {TEXT4}
-                       </p>
-                       </div>
+                    <h3 class="accordion-header">
+                        {TEXT4}
+                    <span class="acc-icon"></span>
+                    </h3>
+                    <div class="accordion-content">
+                        <p>
+                        {TEXT5}
+                        </p>
+                    </div>
                 </div>'
             );
         })
