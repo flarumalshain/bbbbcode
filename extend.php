a92954068041af0db5ae5b95eeeb8c7cca1563d7
Bungalow Bill <41351317+zerosonesfun@users.noreply.github.com>
2019-02-24 01:48:55 -0500

    Update extend.php

--- extend.php
+++ extend.php
@@ -3,8 +3,8 @@
 /*
  * This file is part of Flarum.
  *
- * (c) Toby Zerner <toby.zerner@gmail.com>
- *
+ * (c) Toby Zerner <toby.zerner@gmail.com> (Flarum)
+ * (c) Two Narwhals (bbbbcode extension)
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
