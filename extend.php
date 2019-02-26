00e97442bfe1cc446047d77355659db018c01bd6
Bungalow Bill <41351317+zerosonesfun@users.noreply.github.com>
2019-02-26 16:19:31 -0500

    Update extend.php

--- extend.php
+++ extend.php
@@ -58,7 +58,7 @@ return [
                 '<span class="{TEXT7}"></span>'
             );
             $config->BBCodes->addCustom(
-                '[tour5 linktext="{TEXT8}" header1="{TEXT9}" content1="{TEXT10}" header2="{TEXT11}" content2="{TEXT12}" header3="{TEXT13}" content3="{TEXT14}" header4="{TEXT15}" content4="{TEXT16}" header5="{TEXT17}" content5="{TEXT18}"]',
+                '[pop5 linktext="{TEXT8}" header1="{TEXT9}" content1="{TEXT10}" header2="{TEXT11}" content2="{TEXT12}" header3="{TEXT13}" content3="{TEXT14}" header4="{TEXT15}" content4="{TEXT16}" header5="{TEXT17}" content5="{TEXT18}"]',
                 '<a href="#modal-stackable">{TEXT8}</a>
                     <section class="css-modal" id="modal-stackable" tabindex="-1" role="dialog" aria-labelledby="stackable-label" aria-hidden="true">
 
@@ -130,6 +130,24 @@ return [
 
                        <div class="css-modal_content">
                        <p>{Text18}</p></div>
+                       </div>
+
+                       <a href="#!" class="css-modal_close" title="Close this modal" data-dismiss="modal" data-close="Close">&times;</a>
+                    </section>'
+            );
+             $config->BBCodes->addCustom(
+               '[pop linktext="{TEXT19}" header="{TEXT20}" content="{TEXT21}"]',
+               '<a href="#modal-stackable">{TEXT19}</a>
+                    <section class="css-modal" id="modal-stackable" tabindex="-1" role="dialog" aria-labelledby="stackable-label" aria-hidden="true">
+
+                       <div class="css-modal_inner">
+                       <header class="css-modal_header">
+                       <h2 id="stackable-label">{TEXT20}</h2>
+                       </header>
+
+                       <div class="css-modal_content">
+                       <p>{TEXT21}</p>
+                       </div>
                        </div>
 
                        <a href="#!" class="css-modal_close" title="Close this modal" data-dismiss="modal" data-close="Close">&times;</a>
