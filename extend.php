ecea8b47d145688278a5e138ea2ab7801bb26ccb
Bungalow Bill <41351317+zerosonesfun@users.noreply.github.com>
2019-07-13 00:26:05 -0400

    Update extend.php

--- extend.php
+++ extend.php
@@ -164,5 +164,33 @@ return [
                        <a href="#!" class="css-modal_close" title="Close this modal" data-dismiss="modal" data-close="Close">&times;</a>
                     </section>'
             );
+             $config->BBCodes->addCustom(
+               '[audio mp3="{TEXT22}" ogg="{TEXT23}"]',
+               '            <audio controls>
+  <source src="{TEXT22}" type="audio/mpeg">
+  <source src="{TEXT23}" type="audio/ogg">
+  <!-- fallback for non supporting browsers goes here -->
+  <p>Your browser does not support HTML5 audio, but you can still 
+     <a href="{TEXT22}">download the music</a>.</p>
+</audio>'
+            );
+             $config->BBCodes->addCustom(
+               '[audio mp3="{TEXT24}"]',
+               '            <audio controls>
+  <source src="{TEXT24}" type="audio/mpeg">
+  <!-- fallback for non supporting browsers goes here -->
+  <p>Your browser does not support HTML5 audio, but you can still 
+     <a href="{TEXT24}">download the music</a>.</p>
+</audio>'
+            );
+             $config->BBCodes->addCustom(
+               '[audio ogg="{TEXT25}"]',
+               '            <audio controls>
+  <source src="{TEXT25}" type="audio/ogg">
+  <!-- fallback for non supporting browsers goes here -->
+  <p>Your browser does not support HTML5 audio, but you can still 
+     <a href="{TEXT25}">download the music</a>.</p>
+</audio>'
+            );
         })
 ];
