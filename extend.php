696270c1f7747252a887dbf7b341dd6dbeedb26e
Bungalow Bill <41351317+zerosonesfun@users.noreply.github.com>
2019-08-04 15:33:13 -0400

    Update extend.php

--- extend.php
+++ extend.php
@@ -56,93 +56,6 @@ return [
                 '[animal="{TEXT7}"]',
                 '<span class="{TEXT7}"></span>'
             );
-            $config->BBCodes->addCustom(
-                '[pop5 linktext="{TEXT8}" header1="{TEXT9}" content1="{TEXT10}" header2="{TEXT11}" content2="{TEXT12}" header3="{TEXT13}" content3="{TEXT14}" header4="{TEXT15}" content4="{TEXT16}" header5="{TEXT17}" content5="{TEXT18}"]',
-                '<a href="#modal-stackable">{TEXT8}</a>
-                    <section class="css-modal" id="modal-stackable" tabindex="-1" role="dialog" aria-labelledby="stackable-label" aria-hidden="true">
-
-                       <div class="css-modal_inner">
-                       <header class="css-modal_header">
-                       <h2 id="stackable-label">{TEXT9}</h2>
-                       </header>
-
-                       <div class="css-modal_content">
-                       <p>{TEXT10}</p>
-                       </div>
-                         <div class="css-modal_footer">
-                         <p><a href="">Close</a> <a href="#modal-stackable-2" class="css-modal_button">Next</a></p></div>
-                       </div>
-
-                       <a href="" class="css-modal_close" title="Close this modal" data-dismiss="modal" data-close="Close">&times;</a>
-                    </section>
-
-                    <section class="css-modal" id="modal-stackable-2" tabindex="-1" role="dialog" aria-labelledby="stackable-label" aria-hidden="true">
-
-                       <div class="css-modal_inner">
-                       <header class="css-modal_header">
-                       <h2 id="stackable-label">{TEXT11}</h2>
-                       </header>
-
-                       <div class="css-modal_content">
-                       <p>{TEXT12}</p>
-                       </div>
-                         <div class="css-modal_footer">
-                         <p><a href="">Close</a> <a href="#modal-stackable-3" class="css-modal_button">Next</a></p></div>
-                       </div>
-
-                       <a href="close-pop" class="css-modal_close" title="Close this modal" data-dismiss="modal" data-close="Close">&times;</a>
-                    </section>
-
-                    <section class="css-modal" id="modal-stackable-3" tabindex="-1" role="dialog" aria-labelledby="stackable-label" aria-hidden="true">
-
-                       <div class="css-modal_inner">
-                       <header class="css-modal_header">
-                       <h2 id="stackable-label">{TEXT13}</h2>
-                       </header>
-
-                       <div class="css-modal_content">
-                       <p>{TEXT14}</p>
-                       </div>
-                         <div class="css-modal_footer">
-                         <p><a href="">Close</a> <a href="#modal-stackable-4" class="css-modal_button">Next</a></p></div>
-                       </div>
-
-                       <a href="" class="css-modal_close" title="Close this modal" data-dismiss="modal" data-close="Close">&times;</a>
-                       </section>
-
-                    <section class="css-modal" id="modal-stackable-4" tabindex="-1" role="dialog" aria-labelledby="stackable-label" aria-hidden="true">
-
-                       <div class="css-modal_inner">
-                       <header class="css-modal_header">
-                       <h2 id="stackable-label">{TEXT15}</h2>
-                       </header>
-
-                       <div class="css-modal_content">
-                       <p>{TEXT16}</p>
-                       </div>
-                         <div class="css-modal_footer">
-                         <p><a href="">Close</a> <a href="#modal-stackable-5" class="css-modal_button">Next</a></p></div>
-                       </div>
-
-                       <a href="" class="css-modal_close" title="Close this modal" data-dismiss="modal" data-close="Close">&times;</a>
-                    </section>
-
-                    <section class="css-modal" id="modal-stackable-5" tabindex="-1" role="dialog" aria-labelledby="stackable-label" aria-hidden="true">
-
-                       <div class="css-modal_inner">
-                       <header class="css-modal_header">
-                       <h2 id="stackable-label">{TEXT17}</h2>
-                       </header>
-
-                       <div class="css-modal_content">
-                       <p>{TEXT18}</p></div>
-                         <div class="css-modal_footer">
-                         <p><a href="">Close</a></p></div>
-                       </div>
-
-                       <a href="" class="css-modal_close" title="Close this modal" data-dismiss="modal" data-close="Close">&times;</a>
-                    </section>'
-            );
              $config->BBCodes->addCustom(
                '[pop linktext="{TEXT19}" header="{TEXT20}" content="{TEXT21}"]',
                '<a href="#modal-stackable-not">{TEXT19}</a>
@@ -157,10 +70,10 @@ return [
                        <p>{TEXT21}</p>
                        </div>
                        <div class="css-modal_footer">
-                         <p><a href="" class="css-modal_button">Close</a></p></div>
+                         <p><a href="" onclick="return false" class="css-modal_button">Close</a></p></div>
                        </div>
 
-                       <a href="" class="css-modal_close" title="Close this modal" data-dismiss="modal" data-close="Close">&times;</a>
+                       <a href="" onclick="return false" class="css-modal_close" title="Close this modal" data-dismiss="modal" data-close="Close">&times;</a>
                     </section>'
             );
              $config->BBCodes->addCustom(
